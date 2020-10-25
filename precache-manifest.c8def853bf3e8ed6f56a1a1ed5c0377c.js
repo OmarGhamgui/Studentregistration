e02e3df93ce8aca669d7052f2685e88efb9b7c7d
OmarGhamgui <70692545+OmarGhamgui@users.noreply.github.com>
2020-10-25 12:15:49 +0100

    Updates

---
+++ precache-manifest.c8def853bf3e8ed6f56a1a1ed5c0377c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc5e044da4fc299c8aa7df4253f4a09",
+    "url": "/Studentregistration/index.html"
+  },
+  {
+    "revision": "7ced18a809a008f9d375",
+    "url": "/Studentregistration/static/css/main.76c28d41.chunk.css"
+  },
+  {
+    "revision": "37db3943de8177f00df4",
+    "url": "/Studentregistration/static/js/2.81532bce.chunk.js"
+  },
+  {
+    "revision": "78fd0f62364854e39c53711d027f73a3",
+    "url": "/Studentregistration/static/js/2.81532bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ced18a809a008f9d375",
+    "url": "/Studentregistration/static/js/main.5b23d12c.chunk.js"
+  },
+  {
+    "revision": "4b71267939e84d1e2125",
+    "url": "/Studentregistration/static/js/runtime-main.0bb7a566.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Studentregistration/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Studentregistration/precache-manifest.c8def853bf3e8ed6f56a1a1ed5c0377c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Studentregistration/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
